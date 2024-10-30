487cd4da156f4c7f69c63b7aa00187d9d0b961c3
AlreadyBold <junheong@nate.com>
2024-10-30 15:03:43 +0900

    config: mongoDB Connect

--- study/src/main/java/com/websocket/study/StudyApplication.java
+++ study/src/main/java/com/websocket/study/StudyApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudyApplication {
 
+	/*@EnableJpaRepositories(basePackages = {
+		"com.challenge.chat.domain.chat.repository.mysql",
+		"com.challenge.chat.domain.member.repository"
+	})
+	@EnableMongoRepositories(basePackages = "com.challenge.chat.domain.chat.repository.mongo")*/
 	public static void main(String[] args) {
 		SpringApplication.run(StudyApplication.class, args);
 	}
